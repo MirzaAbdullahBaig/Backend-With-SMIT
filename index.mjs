4102dc24d44eadafd628caabb314a8b3f2d6dd23
Mirza Abdullah Baig <abaig7921@gmail.com>
2024-12-25 18:46:59 +0500

    Backend 2nd Class

--- index.mjs
+++ index.mjs
@@ -1,23 +1,32 @@
 import express from 'express'
-import { products } from "./productData.mjs"
+// import { products } from "./productData.mjs"
 import cors from "cors"
 
 const app = express()
-const port = 3000
+const port = process.env.port || 3000
+const products = []
 
 app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, response) => {
   response.send("Hello Abdullah!")
 })
 
-// const student = (req, response) => {
-//   response.send({ name: "Abdul-Qadir", age: 22 })
-// }
-
 app.get('/products', (req, response) => {
-  // response.send({ name: "Abdul-Qadir", age: 21 })
   response.send(products)
+})
+
+app.post('/product', (req, response) => {
+  console.log("request", req.body);
+  const product = req.body
+  products.push(product)
+
+  response.send("product added successfully")
+})
+
+app.use((req, response) => {
+  response.send("Page not Found")
 })
 
 app.listen(port, () => {
